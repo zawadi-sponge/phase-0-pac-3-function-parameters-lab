d28642bd5301b1cfc51e9619c458cc9848495049
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-04 12:14:08 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,19 @@
+const name ="Zawadi";
+const language ="Javascript"
+
+
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
